63146b7fae832db221781cd96456adb4d8374638
Denzel <dbartolaba123@gmail.com>
2022-04-05 11:21:21 -0700

    got my classes to work

---
+++ sandbox.js
@@ -0,0 +1,72 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    return this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+    
+  peek() {
+    return this.stack[this.length - 1];
+  }
+
+  get length() {
+    return this.stack.length;
+  }
+
+  isEmpty() {
+    return this.length === null;
+  }
+
+}
+const stack = new Stack();
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
+console.log(stack.pop()); // 'lizard'
+console.log(stack.peek()); // 'goose'
+console.log(stack.pop()); // 'goose'
+stack.push('llama');
+console.log(stack.pop()); // 'llama'
+console.log(stack.peek()); // 'fox'
+console.log(stack.pop()); // 'fox'
+console.log(stack.pop()); // null
+
+
+class Queue {
+  queue = [];
+
+  constructor(initialQueue) {
+    if(initialQueue) this.queue = initialQueue;
+  }
+
+  enqueue(item) {
+    return this.queue.unshift(item);
+  }
+
+  dequeue() {
+    return this.queue.pop();
+  }
+
+  hasNext() {
+    return this.queue.length < 1 ? false : true;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // 'goose'
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // 'fox'
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.dequeue()); // null
